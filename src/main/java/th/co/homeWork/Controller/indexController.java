e07e68f89af1eea747104ec39ba26b63f1d239d2
pchomeian <ooo123coll@gmail.com>
2019-09-25 14:20:25 +0800

    first

---
+++ src/main/java/th/co/homeWork/Controller/indexController.java
@@ -0,0 +1,14 @@
+package th.co.homeWork.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class indexController {
+
+	@RequestMapping(value="/")
+	public String index() {
+		System.out.println("-index-");
+		return "index";
+	}
+}
---
+++ src/main/java/th/co/homeWork/HomeWorkApplication.java
@@ -0,0 +1,14 @@
+package th.co.homeWork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWorkApplication.class, args);
+	}
+
+}
+ 
